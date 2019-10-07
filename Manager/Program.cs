05e820c0cc58fd1a3cfd215ccce3ed0276995caf
Yao Ming Huang <yaoming.huang@ischool.com.tw>
2019-10-07 10:13:52 +0800

    升級 .Net Framework、TLS 版本。

--- Manager/Program.cs
+++ Manager/Program.cs
@@ -90,6 +90,8 @@ namespace Manager
             //    return;
             //}
 
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
             ServicePointManager.ServerCertificateValidationCallback = delegate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
             {
                 return true;
